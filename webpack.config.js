fba4c4a51c6a3a22db709cb40e63cdb6ce43407b
runohod <andrei.rumynthev1728@yandex.ru>
2024-05-07 15:43:49 +0300

    подключение html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // подключили плагин
+
 
 module.exports = {
     entry: { main: './src/index.js' },
@@ -26,5 +29,11 @@ module.exports = {
         exclude: '/node_modules/'
       }
       ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin(), // использовали плагин
+  ]
 };
